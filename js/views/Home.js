cadb45abf0e54d6b286ac718d17197831e0fb286
Denzel-Britton <denzel.britton573@gmail.com>
2022-07-26 12:34:01 -0500

    added movies titles

--- js/views/Home.js
+++ js/views/Home.js
@@ -4,27 +4,47 @@ import {getUser} from "../auth.js";
 const BASE_URI = `${BACKEND_HOST}/api/s3/download`;
 
 export default function Home(props) {
-    return `
-        <header>
-            <h1>Home Page</h1>
-        </header>
-        <main>
-            <div>
-                <p>
-                    This is the home page text.
-                </p>    
-            </div>
-        </main>
-    `;
+    console.log(props);
+
+    let html = `
+    <h1>Movies</h1>
+    <table id="my-quotes " class="table  table-striped"> `;
+
+    //add a table row for each table element
+    for (let i = 0; i < props.movies.length; i++) {
+        html += `
+        <tr>
+        <td>
+        "${props.movies[i].title}"  
+       
+</td>
+</tr>
+ <tr>
+        <td>
+      
+       
+</td>
+</tr>
+
+        `
+    }
+    html+= `
+   </table>
+  
+`;
+
+    return  html
+
 }
 
 export function HomeEvents() {
-    // TODO: use an enum for message type
-    // const authority = getUserRole();
-    const user = getUser();
-    if(!user) {
-        showNotification("Welcome visitor", "secondary");
-    } else {
-        showNotification("Welcome " + user.userName, "info");
-    }
+    // // TODO: use an enum for message type
+    // // const authority = getUserRole();
+    // const user = getUser();
+    // if(!user) {
+    //     showNotification("Welcome visitor", "secondary");
+    // } else {
+    //     showNotification("Welcome " + user.userName, "info");
+    // }
+
 }
